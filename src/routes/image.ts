685f13aa5f5de40df61d1786f8500223b131cc2c
47JMC <mjabir.5b@gmail.com>
2026-01-24 21:58:07 +0530

    Change Content-Type header in /view-image to 'image/png'

--- src/routes/image.ts
+++ src/routes/image.ts
@@ -82,6 +82,7 @@ router.get("/view-image/:id.png", (req, res) => {
     return res.status(404).send("Not found");
   }
 
+  res.header("Content-Type", "image/png");
   res.sendFile(filePath);
 });
 
